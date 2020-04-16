dca6e91b4d2027677d82faac92f8b9d555992014
bimbimprasetyoafif <bimo.prasetyoafif@gmail.com>
2020-04-16 16:59:13 +0700

    Update model

--- film/models.py
+++ film/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Film(models.Model):
     judul = models.CharField(max_length=25)
     tahun = models.IntegerField()
@@ -12,4 +11,4 @@ class Gambar(models.Model):
     gambar = models.FileField(blank=False, null=False)
 
     def __str__(self):
-        return self.ProductImage.name
+        return self.gambar.name
